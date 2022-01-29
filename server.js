886274f4c2f172ffaf4973566fdb294e6999d1cc
bmatusiak <bmatusiak@gmail.com>
2022-01-29 14:46:39 +0000

    update

--- server.js
+++ server.js
@@ -2,34 +2,31 @@ var hash = "igSBrdYKihT4nD5ggix/U4Snrpk+NjDT05xCZXK8=2345678912345678901"
 //   +(new Date().getTime());
 
 
-
-var pair_master = {
-    pub: 'C6HWH-uPo9I2p6mjXwfiqhjRfl139vwAA7Tc7F3D96g.wMURUl83fZAGtDnQ0fHSpIVzc9OmLGbKyZKcv06KI9A',
-    priv: 'qNHbTGXOp6YoyICIq7C_e9g6Q3Wrh0pPFqFmi2iPHw4',
-    epub: 'Qz8QvX7Wty6W-upWZYj5R-6dzCIutjs0G4Agrpxx2g0.KyD33PA0CtWiAvajJcdb--s6e1LEs12Tt681r4R9bsM',
-    epriv: 'V8dSEd9eNU-FMoBwzxlMlBYWOBgp1hveImhpx-876Wo'
-};
-
-var pair_slave = {
-    pub: 'VIVbp2P8tGy4rggF79ncqqEmfSWAGaxsZmRIAkICKJg.APRx3zArF0rB4O8jZa7ZfeQtR2pd4x75Sa3_9gsMmyI',
-    priv: 'yQgGsG6Yefy0sLW5_vpIYeV3UycpObI0NrHpJSxemvo',
-    epub: 'dgO6ExzCTZfP8jHVx2lgUpMWYHt5nZWvUcbW76w2s20.p1Iu2FYUxI7A-tAGhkmdiT9SJJq4Zlafg9FG1KnFoZU',
-    epriv: 'P0non47qwbLvrl1VjiazZyPEa7TdGy87AG5I8ALO7DA'
-}
-
-var gunDC;
-if (process.env.INITIATOR) {
-    gunDC = require("./index.js")(true, hash, pair_slave);
-}
-else {
-    gunDC = require("./index.js")(false, hash, pair_master);
-}
-
-gunDC.on("connected",function(socket){
+;(async function() {
     
-    socket.on("test",function(val){
-        console.log("test",val);
+        
+    var GUN = require('gun');
+    var SEA = require('gun/sea');
+
+    var pair_master = await SEA.pair();;
+
+    var pair_slave = await SEA.pair();
+
+    var gunDC;
+    if (process.env.INITIATOR) {
+        gunDC = require("./index.js")(true, hash, pair_slave);
+    }
+    else {
+        gunDC = require("./index.js")(false, hash, pair_master);
+    }
+
+    gunDC.on("connected", function(socket) {
+
+        socket.on("test", function(val) {
+            console.log("test", val);
+        });
+
+        socket.emit("test", process.env.INITIATOR || false);
     });
 
-    socket.emit("test",process.env.INITIATOR || false);
-});
+})();
