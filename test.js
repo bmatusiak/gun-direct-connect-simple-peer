41c5f7c6e65db37295e0c9a4c46e42e17da03512
bmatusiak <bmatusiak@gmail.com>
2022-01-31 01:34:46 +0000

    update

--- test.js
+++ test.js
@@ -105,9 +105,9 @@
 
     function exitHandler(options, exitCode) {
         // if (options.cleanup){
-        //   $log('clean');
+        //   console.log('clean');
         // } 
-        // if (exitCode || exitCode === 0) $log("exitCode",exitCode);
+        // if (exitCode || exitCode === 0)  console.log("exitCode",exitCode);
         if (options.exit) {
             gunDC.destroy();
             process.exit();
@@ -116,14 +116,11 @@
 
     //do something when app is closing
     process.on('exit', exitHandler.bind(null, { cleanup: true }));
-
     //catches ctrl+c event
     process.on('SIGINT', exitHandler.bind(null, { exit: true }));
-
     // catches "kill pid" (for example: nodemon restart)
     process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
     process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
-
     //catches uncaught exceptions
     process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
 })();
